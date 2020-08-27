53892ab1cd010d652a8093bb42e36cf86cd2fb58
Showmick Kar <showmick12@gmail.com>
2020-08-27 22:39:27 +0600

    Create Bit Strings.cpp

---
+++ Introductory Problems/Bit Strings.cpp
@@ -0,0 +1,43 @@
+Problem link: https://cses.fi/problemset/task/1617/
+
+#include <bits/stdc++.h>
+#define INF 1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define arr array
+#define _min(a) *min_element(a.begin(), a.end());
+#define _max(a) *max_element(a.begin(), a.end());
+#define _sort(x) sort(x.begin(), x.end())
+#define _rsort(x) sort(x.rbegin(), x.rend())
+#define _reverse(x) reverse(x.begin(), x.end())
+#define _debug(x) cout << #x << " = " << x << endl;
+#define debug(x, y) cout << #x " = " << x << " " << #y << " = " << y << endl;
+#define _lower_bound(a, x) lower_bound(a.begin(), a.end(), x) - a.begin()
+#define _upper_bound(a, x) upper_bound(a.begin(), a.end(), x) - a.begin()
+typedef long long ll;
+using namespace std;
+
+long long power(long long a, long long b) {
+    if (b == 0) return 1;
+    long long temp = power(a, b/2);
+    long long result = temp * temp;
+    result %= MOD;
+    if (b % 2 == 1) result *= a;
+    result %= MOD;
+    return result;
+}
+
+void solve() {	
+	int n; cin >> n;
+	cout << power(2, n) << endl;
+}   
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    // int t; cin >> t;
+    // while(t--) 
+        solve();
+}
